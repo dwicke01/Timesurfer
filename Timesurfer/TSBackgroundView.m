4cdde2e9b97e1274387e2b9960241ca4de6b7628
dwicke01 <dwicke01@students.poly.edu>
2015-06-26 14:42:23 -0400

    Changed background to CAGradientLayer

--- Timesurfer/TSBackgroundView.m
+++ Timesurfer/TSBackgroundView.m
@@ -10,40 +10,20 @@
 
 @implementation TSBackgroundView
 
-
-- (void)drawRect:(CGRect)rect {
-    
-    CGRect parentContainer = [super bounds];
-    
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    
-    UIColor* color1 = [UIColor colorWithRed:148./255. green:186./255. blue:101./255. alpha:0.99];
-    UIColor* color2 = [UIColor colorWithRed:39./255. green:144./255. blue:176./255. alpha:1.];
-    
-    UIColor* color3 = [UIColor colorWithRed: 0.547 green: 0 blue: 0.084 alpha: 0.373];
-    UIColor* color4 = [UIColor colorWithRed: 0.939 green: 0.218 blue: 0.302 alpha: 0.627];
-
-    CGFloat linearGradient1Locations[] = {0, 1};
-    CGGradientRef linearGradient1 = CGGradientCreateWithColors(colorSpace, (__bridge CFArrayRef)@[(id)color2.CGColor, (id)color1.CGColor], linearGradient1Locations);
-
-    CGFloat linearGradient2Locations[] = {0, 1};
-    CGGradientRef linearGradient2 = CGGradientCreateWithColors(colorSpace, (__bridge CFArrayRef)@[(id)color3.CGColor, (id)color4.CGColor], linearGradient2Locations);
-
-    UIBezierPath* path3Path = [UIBezierPath bezierPathWithRect: parentContainer];
-    CGContextSaveGState(context);
-    [path3Path addClip];
-    CGContextDrawLinearGradient(context, linearGradient1,
-                                CGPointMake(0, 0),
-                                CGPointMake(0, 740),
-                                kCGGradientDrawsBeforeStartLocation | kCGGradientDrawsAfterEndLocation);
-    CGContextRestoreGState(context);
-    
-    CGGradientRelease(linearGradient2);
-    CGGradientRelease(linearGradient1);
-    CGColorSpaceRelease(colorSpace);
-    
-
+-(instancetype)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder:aDecoder]) {
+        CAGradientLayer *layer = [[CAGradientLayer alloc] init];
+        layer.frame = [self bounds];
+        layer.colors = @[[self cgColorForRed:148 green:186 blue:101 alpha:1], [self cgColorForRed:39 green:144 blue:176 alpha:1]];
+        layer.startPoint = CGPointMake(0, 1);
+        layer.endPoint = CGPointMake(0, 0);
+        [self.layer insertSublayer:layer atIndex:0];
+    }
+    return self;
 }
 
+-(id) cgColorForRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha {
+    UIColor *color = [UIColor colorWithRed:red/255.0 green:green/255.0 blue:blue/255.0 alpha:alpha];
+    return (id)color.CGColor;
+}
 @end
