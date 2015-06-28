b1b2a7730369ad868b2db3f834393fd9e4288b7b
dwicke01 <dwicke01@students.poly.edu>
2015-06-28 09:10:01 -0400

    Committing fade animation

--- Timesurfer/TSBackgroundView.m
+++ Timesurfer/TSBackgroundView.m
@@ -9,6 +9,7 @@
 #import "TSBackgroundView.h"
 
 @implementation TSBackgroundView {
+    CAGradientLayer *daytimeLayer;
     NSArray   *_daytimeColors;
     NSArray   *_nighttimeColors;
     NSUInteger _counter;
@@ -23,18 +24,37 @@
         _daytimeColors = @[[self cgColorForRed:148 green:186 blue:101 alpha:1], [self cgColorForRed:137 green:181 blue:108 alpha:1], [self cgColorForRed:126 green:177 blue:116 alpha:1], [self cgColorForRed:115 green:173 blue:123 alpha:1], [self cgColorForRed:104 green:169 blue:131 alpha:1], [self cgColorForRed:93 green:165 blue:138 alpha:1], [self cgColorForRed:82 green:160 blue:148 alpha:1], [self cgColorForRed:71 green:156 blue:153 alpha:1], [self cgColorForRed:60 green:152 blue:161 alpha:1], [self cgColorForRed:49 green:148 blue:168 alpha:1], [self cgColorForRed:39 green:144 blue:176 alpha:1]];
         _nighttimeColors = @[[self cgColorForRed:39 green:144 blue:176 alpha:1], [self cgColorForRed:36 green:136 blue:166 alpha:1], [self cgColorForRed:34 green:128 blue:157 alpha:1], [self cgColorForRed:32 green:120 blue:147 alpha:1], [self cgColorForRed:30 green:113 blue:138 alpha:1], [self cgColorForRed:28 green:105 blue:128 alpha:1], [self cgColorForRed:26 green:97 blue:119 alpha:1], [self cgColorForRed:24 green:90 blue:110 alpha:1], [self cgColorForRed:22 green:82 blue:100 alpha:1], [self cgColorForRed:20 green:74 blue:91 alpha:1], [self cgColorForRed:18 green:67 blue:82 alpha:1]];
         _counter = 0;
-        CAGradientLayer *layer = (id)self.layer;
-        layer.frame = [self bounds];
-        layer.colors = _daytimeColors;
+//        CAGradientLayer *layer = (id)self.layer;
+//        layer.frame = [self bounds];
+//        layer.colors = _daytimeColors;
+//        
+//        layer.startPoint = CGPointMake(0, 1);
+//        layer.endPoint = CGPointMake(0, 0);
         
-        layer.startPoint = CGPointMake(0, 1);
-        layer.endPoint = CGPointMake(0, 0);
+        //CAGradientLayer *daytimeLayer = [[CAGradientLayer alloc] init];
+        daytimeLayer = [[CAGradientLayer alloc]init];
+        daytimeLayer.frame = [self bounds];
+        daytimeLayer.startPoint = CGPointMake(0, 1);
+        daytimeLayer.endPoint = CGPointMake(0, 0);
+        daytimeLayer.colors = _daytimeColors;
+        
+        CAGradientLayer *nighttimeLayer = [[CAGradientLayer alloc] init];
+        nighttimeLayer.frame = [self bounds];
+        nighttimeLayer.startPoint = CGPointMake(0, 1);
+        nighttimeLayer.endPoint = CGPointMake(0, 0);
+        nighttimeLayer.colors = _nighttimeColors;
+        
+        [self.layer addSublayer:nighttimeLayer];
+        [self.layer addSublayer:daytimeLayer];
+        
         self.maskLayer = [CALayer layer];
         [self.maskLayer setFrame:CGRectMake(0, 0, 0, self.frame.size.height)];
         [self.maskLayer setBackgroundColor:[[UIColor blackColor] CGColor]];
-        [self performAnimation];
-        [layer setMask:self.maskLayer];
-        [self.layer insertSublayer:self.maskLayer atIndex:0];
+        
+        [self makeDaytimeLayerTransparent];
+        //[self performAnimation];
+        //[layer setMask:self.maskLayer];
+        //[self.layer insertSublayer:self.maskLayer atIndex:0];
     }
     return self;
 }
@@ -42,6 +62,23 @@
 -(id) cgColorForRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha {
     UIColor *color = [UIColor colorWithRed:red/255.0 green:green/255.0 blue:blue/255.0 alpha:alpha];
     return (id)color.CGColor;
+}
+
+- (void)makeDaytimeLayerTransparent {
+    CAGradientLayer *layer = daytimeLayer; //self.layer.sublayers[[self.layer.sublayers count]-1];
+    CABasicAnimation* fadeAnim = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    fadeAnim.fromValue = [NSNumber numberWithFloat:1.0];
+    fadeAnim.toValue = [NSNumber numberWithFloat:0.0];
+    fadeAnim.duration = 1.0;
+    [layer addAnimation:fadeAnim forKey:@"opacity"];
+    layer.opacity = 0;
+//    CABasicAnimation *animation;
+//    animation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+//    [animation setToValue:[NSNumber numberWithDouble:(layer.opacity - .1)]];
+//    [animation setDuration:.16];
+//    [animation setRemovedOnCompletion:YES];
+//    [animation setDelegate:self];
+//    [layer addAnimation:animation forKey:@"opacity"];
 }
 
 - (void)performAnimation {
@@ -69,6 +106,7 @@
 }
 
 - (void)animationDidStop:(CAAnimation *)animation finished:(BOOL)flag {
+    return;
     CAGradientLayer *layer = (id)self.layer;
     if (![((UIColor*)layer.colors[10]) isEqual:[self cgColorForRed:39 green:144 blue:176 alpha:1]])
         [self performAnimation];
