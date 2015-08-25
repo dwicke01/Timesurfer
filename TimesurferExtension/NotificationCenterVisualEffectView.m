c5a94ed46e6bbc116b3c2f2cf4947e4e1079314e
dwicke01 <dwicke01@students.poly.edu>
2015-08-25 17:43:45 -0400

    created files for background

---
+++ TimesurferExtension/NotificationCenterVisualEffectView.m
@@ -0,0 +1,13 @@
+//
+//  NotificationCenterVisualEffectView.m
+//  Timesurfer
+//
+//  Created by Daniel Wickes on 8/25/15.
+//  Copyright (c) 2015 gugges. All rights reserved.
+//
+
+#import "NotificationCenterVisualEffectView.h"
+
+@implementation NotificationCenterVisualEffectView
+
+@end
